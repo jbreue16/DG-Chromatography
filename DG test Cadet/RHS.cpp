5c65da08014fc7317b1acff3bb40000066744f33
Jan Breuer <j.m.br@gmx.de>
2021-11-07 17:59:01 +0100

    add isotherm Jacobian and c to w resubstitution to Semidiscretization

--- DG test Cadet/RHS.cpp
+++ DG test Cadet/RHS.cpp
@@ -94,6 +94,65 @@ void calcH(Container& cache, ParameterProvider& para) {
 }
 
 /**
+* @brief returns (F* dq/dc + I)^-1
+* @param [in] cell index of the cell
+*/
+MatrixXd calcDQDC(Container& cache, ParameterProvider& para, int cell, int node) {
+	//TODO calculation depending on model isotherm
+	MatrixXd Jac = MatrixXd::Zero(para.nComp, para.nComp);
+	if (para.isotherm == "Langmuir") {
+		// first calc (1 + sum(b_i * c_i))^-1
+		double factor = 1.0;
+		for (int comp = 0; comp < para.nComp; comp++) {
+			factor += para.ADratio[comp] * cache.c[cell * para.strideCell() + node * para.strideNode() + comp];
+		}
+		factor = 1.0 / factor;
+		// estimate Jacobian dq/dc + I
+		for (int i = 0; i < para.nComp; i++) {
+			for (int j = 0; j < para.nComp; j++) {
+				Jac(i, j) = para.porosity * (- (pow(factor, 2)) * para.adsorption[i] * para.ADratio[j] * cache.c[cell * para.strideCell() + node * para.strideNode() + i * para.strideComp()]);
+				if (i == j) {
+					Jac(i, j) += para.porosity * (para.adsorption[i] * factor) + 1; // +1 for F*Jac + I
+				}
+			}
+		}
+	}
+	else {
+		throw std::invalid_argument("spelling error or this isotherm is not implemented yet");
+	}
+	return Jac.inverse();
+}
+
+/**
+* @brief calculates c from w
+*/
+void calcC(Container& cache, ParameterProvider& para) {
+	if (para.isotherm == "Linear") { // diagonal Jacobian dq/dc
+		for (int comp = 0; comp < para.nComp; comp++) {
+			double factor = 1.0 / (1.0 + para.porosity * para.adsorption[comp]);
+			for (int nodes = 0; nodes < para.nCells * (para.polyDeg + 1); nodes++) {
+				cache.dc[nodes * para.strideNode() + comp] = factor * cache.w[nodes * para.strideNode() + comp];
+			}
+		}
+	}
+	else { // Dense Jacobian dq/dc
+		for (int cell = 0; cell < para.nCells; cell++) {
+			for (int node = 0; node < para.polyDeg + 1; node++) {
+				// get (I + F* J)^-1 for each node, J=dq/dc being the isotherm Jacobian
+				MatrixXd Jac = calcDQDC(cache, para, cell, node);
+				// c_components =  (I + F* J)^-1 * w_components
+				for (int comp = 0; comp < para.nComp; comp++) {
+					for (int comp2 = 0; comp2 < para.nComp; comp2++){
+					cache.dc[cell * para.strideCell() + node * para.strideNode() + comp * para.strideComp()]
+						+= Jac(comp, comp2) * cache.w[cell * para.strideCell() + node * para.strideNode() + comp2 * para.strideComp()];
+					}
+				}
+			}
+		}
+	}
+}
+
+/**
 * @brief applies the inverse Jacobian of the mapping
 */
 void applyJacobian(Discretization DG, VectorXd& state) {
@@ -104,8 +163,9 @@ void applyJacobian(Discretization DG, VectorXd& state) {
 *
 */
 void ConvDisp(Container& cache, Discretization& DG, ParameterProvider& para) {
-	// reset du, auxiliary q, and surface flux
+	// reset w, h, auxiliary S, surface flux
 	cache.w.setZero();
+	cache.h.setZero();
 	cache.S.setZero();
 	cache.surfaceFlux.setZero();
 	// TODO: actualize boundary flux values !
@@ -118,9 +178,9 @@ void ConvDisp(Container& cache, Discretization& DG, ParameterProvider& para) {
 
 	// solve dispersion convection part of main equation
 	calcH(cache, para); // calculate the substitute h(S(c), c) = D_ax S(c) - v c
-	volumeIntegral(cache, DG, para, cache.c, cache.w);
-	surfaceIntegral(cache, DG, para, cache.c, cache.w, 0);
+	volumeIntegral(cache, DG, para, cache.h, cache.w);
+	surfaceIntegral(cache, DG, para, cache.h, cache.w, 0);
 	applyJacobian(DG, cache.w);
-
-	
+	// estimate state vector derivative dc of mobile phase from substitues w, h
+	calcC(cache, para);
 }
--- DG test Cadet/RHS.hpp
+++ DG test Cadet/RHS.hpp
@@ -8,3 +8,6 @@ void calcSurfaceFlux(Container& cache, Discretization& DG, ParameterProvider& pa
 void surfaceIntegral(Container& cache, Discretization& DG, ParameterProvider& para, VectorXd& state, VectorXd& stateDer, bool aux);
 void ConvDisp(Container& cache, Discretization& DG, ParameterProvider& para);
 void applyJacobian(Discretization DG, VectorXd& state);
+
+Eigen::MatrixXd calcDQDC(Container& cache, ParameterProvider& para, int cell, int node);
+void calcC(Container& cache, ParameterProvider& para);
