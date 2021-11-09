14a143970d921a7e0a10afd3a357b0fc18e4d2d8
Jan Breuer <j.m.br@gmx.de>
2021-11-09 16:25:14 +0100

    add free stream preserving 4th order Carpenter Kennedy expl. RK

--- DG test Cadet/TimeIntegration.cpp
+++ DG test Cadet/TimeIntegration.cpp
@@ -15,10 +15,13 @@
 *@brief returns timestep for linear advection
 */
 double timestep(Discretization DG, ParameterProvider para, double CFL, double tend) {
-	return CFL* DG.deltaX / abs(para.velocity);
+	// Delta X_eff = (DG.deltaX/DG.nNodes)
+	return CFL* (DG.deltaX/DG.nNodes) / abs(para.velocity);
 }
-
-Eigen::VectorXd solve(Container& cache, Discretization& DG, ParameterProvider& para, double tstart, double tend, double CFL, VectorXd start) {
+/**
+*@brief 3rd order expl. RK
+*/
+Eigen::VectorXd solveRK3(Container& cache, Discretization& DG, ParameterProvider& para, double tstart, double tend, double CFL, VectorXd start) {
 	double t = tstart;
 	double Dt = 0.0;
 	cache.c = start;
@@ -29,7 +32,7 @@ Eigen::VectorXd solve(Container& cache, Discretization& DG, ParameterProvider& p
 	while(t < tend) {
 		// determine current timestep
 		Dt = (timestep(DG, para, CFL, tend) + t > tend) ? tend - t : timestep(DG, para, CFL, tend);
-		std::cout << "Timestep: " << Dt << std::endl;
+		//std::cout << "Timestep: " << Dt << std::endl;
 		// calc convection dispersion rhs
 		ConvDisp(cache, DG, para, t); // stores rhs in cache.dc
 
@@ -43,4 +46,41 @@ Eigen::VectorXd solve(Container& cache, Discretization& DG, ParameterProvider& p
 	return cache.c;
 }
 
+/**
+*@brief Carpenter Kennedy 4th order low storage expl. RK
+*/
+Eigen::VectorXd solveRK4(Container& cache, Discretization& DG, ParameterProvider& para, double tstart, double tend, double CFL, VectorXd start) {
+	double t = tstart;
+	double Dt = 0.0;
+	cache.c = start;
+
+	// RK coefficients A, B, C for each stage
+	VectorXd A(5);
+	A << 0.0, -567301805773 / 1357537059087, -2404267990393 / 2016746695238,
+		-3550918686646 / 2091501179385, -1275806237668 / 842570457699;
+	VectorXd B(5);
+	B << 0.0, 1432997174477 / 9575080441755, 2526269341429 / 6820363962896,
+		2006345519317 / 3224310063776, 2802321613138 / 2924317926251;
+	VectorXd C(5);
+	C << 1432997174477 / 9575080441755, 5161836677717 / 13612068292357,
+		1720146321549 / 2090206949498, 3134564353537 / 4481467310338, 2277821191437 / 14882151754819;
+	VectorXd g = VectorXd::Zero(cache.c.size());
+	while (t < tend) {
+		// determine current timestep
+		Dt = (timestep(DG, para, CFL, tend) + t > tend) ? tend - t : timestep(DG, para, CFL, tend);
+		//std::cout << "Timestep: " << Dt << std::endl;
+		// calc convection dispersion rhs
+		ConvDisp(cache, DG, para, t); // stores rhs in cache.dc
+		g = cache.dc;
+		// expl. 3-stage RK
+		for (int stage = 0; stage < 5; stage++) {
+			ConvDisp(cache, DG, para, t + B[stage] * Dt);
+			g = A[stage] * g + cache.dc;
+			cache.c += C[stage] * Dt * g;
+		}
+
+		t += Dt;
+	}
+	return cache.c;
+}
 
--- DG test Cadet/TimeIntegration.hpp
+++ DG test Cadet/TimeIntegration.hpp
@@ -3,6 +3,6 @@
 #include "RHS.hpp"
 #include<Eigen/Dense>
 
-void calcBoundary(Container& cache, Discretization DG, double t);
 double timestep(Discretization DG, ParameterProvider para, double CFL, double tend);
-Eigen::VectorXd solve(Container& cache, Discretization& DG, ParameterProvider& para, double tstart, double tend, double CFL, VectorXd start);
+Eigen::VectorXd solveRK3(Container& cache, Discretization& DG, ParameterProvider& para, double tstart, double tend, double CFL, VectorXd start);
+Eigen::VectorXd solveRK4(Container& cache, Discretization& DG, ParameterProvider& para, double tstart, double tend, double CFL, VectorXd start);
