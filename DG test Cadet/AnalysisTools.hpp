86763cceea4dbf2f5008d904f3b601620c43aca9
Jan Breuer <j.m.br@gmx.de>
2021-11-14 21:39:03 +0100

    add read/write file

--- DG test Cadet/AnalysisTools.hpp
+++ DG test Cadet/AnalysisTools.hpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <string>
+#include <fstream>
 
 #include "gnuplot-iostream.h"
+
+#include "DGspecific.hpp"
+
+/**
+* @file includes functions for analysis and gnuplot API
+*/
+
 
 /**
 * Plotter API for gnuplot
@@ -24,19 +32,67 @@ public:
 		gp << "set ylabel '" << ylabel << "'\n";
 		return *this;
 	}
-	void line_plot(Eigen::VectorXd data, Eigen::VectorXd xAxis) {
+	void oneLine_plot(Eigen::VectorXd data, Eigen::VectorXd xAxis) {
 		std::vector<boost::tuple<double, double> > pts_C;
 		for (int i = 0; i < data.size(); i++) {
 			pts_C.push_back(boost::make_tuple(xAxis[i], data[i]));
 		}
-		gp << "plot '-' with lines\n";// << gp.file1d(cache.c) << endl;
+		gp << "plot '-' with lines\n";
 		gp.send(pts_C);
 		std::cin.get();
 	}
-	void line_plot(Eigen::VectorXd data, Eigen::VectorXd xAxis, std::string_view title, std::string_view xlabel, std::string_view ylabel) {
+	void twoLine_plot(Eigen::VectorXd data1, Eigen::VectorXd data2, Eigen::VectorXd xAxis) {
+		std::vector<boost::tuple<double, double, double> > pts_C;
+		for (int i = 0; i < data1.size(); i++) {
+			pts_C.push_back(boost::make_tuple(xAxis[i], data1[i], data2[i]));
+		}
+		gp << "plot '-' with lines\n";
+		gp.send(pts_C);
+		std::cin.get();
+	}
+	void oneLine_plot(Eigen::VectorXd data, Eigen::VectorXd xAxis, std::string_view title, std::string_view xlabel, std::string_view ylabel) {
 		this->title(title);
 		this->xlabel(xlabel);
 		this->ylabel(ylabel);
-		this->line_plot(data, xAxis);
+		this->oneLine_plot(data, xAxis);
+	}
+	void twoLine_plot(Eigen::VectorXd data1, Eigen::VectorXd data2, Eigen::VectorXd xAxis, std::string_view title, std::string_view xlabel, std::string_view ylabel) {
+		this->title(title);
+		this->xlabel(xlabel);
+		this->ylabel(ylabel);
+		this->twoLine_plot(data1, data2, xAxis);
 	}
 };
+
+void write_csv(Eigen::VectorXd vec, std::string name) {
+	std::ofstream myFile(name);
+	//myFile << "c" << "\n"; // column name
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		myFile << vec[i] << "\n";
+	}
+	myFile.close();
+}
+Eigen::VectorXd read_csv(std::string name, int size) {
+	std::ifstream myFile(name);
+	if (!myFile.is_open()) throw std::runtime_error("Could not open file");
+	Eigen::VectorXd result(size);
+	int count = 0;
+	while (count < size) {
+		myFile >> result[count];
+		count++;
+	}
+	myFile.close();
+	return result;
+}
+
+/**
+* @brief returns state vector of one component
+*/
+VectorXd getComponent(VectorXd state, ParameterProvider para, int Comp) {
+	VectorXd Component = VectorXd::Zero(state.size() / para.nComp);
+	for (int i = 0; i < para.nCells * para.nNodes; i++) {
+		Component[i] = state[i * para.strideNode() + Comp * para.strideComp()];
+	}
+	return Component;
+}
